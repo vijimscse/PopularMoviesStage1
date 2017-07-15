09b6704acc745c32bc34897d75ee6ca7b8d75de5
VijayaLakshmi I N <vijimscse@gmail.com>
2017-07-15 23:51:09 +0530

    Aligned code

--- app/src/main/java/com/udacity/popularmoviesstage1/utils/Config.java
+++ app/src/main/java/com/udacity/popularmoviesstage1/utils/Config.java
@@ -8,12 +8,8 @@ public interface Config {
     String API_KEY_VALUE = "";
 
     String API_KEY = "api_key";
-
     String POPULAR = "popular";
-
     String TOP_RATED = "top_rated";
-
     String BASE_URL = "http://api.themoviedb.org/3/movie/";
     String IMAGE_BASE_URL = "http://image.tmdb.org/t/p/w185";
-
 }
